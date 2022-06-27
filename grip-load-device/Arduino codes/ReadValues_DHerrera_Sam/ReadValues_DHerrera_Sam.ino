cfc524b6bc53908cfcb529c249092beb53db298a
csamid <85626643+csamid@users.noreply.github.com>
2022-06-27 11:16:32 -0700

    arduino code

---
+++ grip-load-device/Arduino codes/ReadValues_DHerrera_Sam/ReadValues_DHerrera_Sam.ino
@@ -0,0 +1,306 @@
+
+#include "SoftwareSerial.h"
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "MPU6050.h" // not necessary if using MotionApps include file
+
+#include "Hx711.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;  // Objects of MPU
+MPU6050 accl;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+Hx711 scale(A2, A3); // Defining load cell pins
+
+// Goniometer parameters
+#define GoniometerPin A0          // Sensor pin (amplified output)
+int GoniometerValue = 0;          // Raw analog sensor reading
+float GoniometerVoltage = 0;      // Mapped voltage (0V - 5V)
+
+// Futek load cell (static test)
+#define FutekLoadCellPin A1       // Futek load cell input pin (amplified output)
+int FutekLoadCellRawValue = 0;    // Raw analog sensor reading (value between 0 - 1023)
+float FutekLoadCellVoltage = 0;   // Mapped voltage (0V - 5V)
+
+int16_t ax, ay, az; // to store raw accel value on z axis
+long accl_x, accl_y, accl_z;
+int16_t gx, gy, gz;
+long az_avg;
+long az_old;
+int8_t i;
+long resultant;
+int no;
+
+//#define Load_Cell
+#define OUTPUT_Z_ACCEL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+// ================================================================
+// ===               SAM ADDED THIS (running average)               ===
+// ================================================================
+const int numReadings = 200;
+int readings[numReadings];
+int readIndex = 0;
+int total = 0;
+int average = 0; 
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+    //timing declaration
+    unsigned long time;
+    
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+
+  scale.setOffset(26100);  // 26100
+  scale.setScale(268);     // 268
+  
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(38400); // 38400
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // initialize device
+    //Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    while (Serial.available());                 // wait for data
+  
+    devStatus = mpu.dmpInitialize();
+
+ // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(567);
+    mpu.setYGyroOffset(-16);
+    mpu.setZGyroOffset(11);
+    mpu.setZAccelOffset(2023); // 1688 factory default for my test chip
+
+    mpu.setXAccelOffset(-21);
+    mpu.setYAccelOffset(2578);
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        //Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        //Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+//==================================================
+for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+            
+            //Serial.print("Time : \t");
+            time = micros();
+            //Serial.print(time);
+            
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+       
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        //Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+
+        //read raw accel values
+        accl.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+            
+            accl_x = ax;
+            accl_y = ay;
+            accl_z = az;
+
+        #ifdef OUTPUT_Z_ACCEL
+        //Serial.print("\tRaw Az, Ay, Ax\t");
+        //az_avg = az/2048;
+ 
+        resultant = sqrt((accl_x*accl_x) + (accl_y*accl_y) + (accl_z*accl_z)); // Obtaining resultant acceleration
+
+        // Read goniomter pin & map to a voltage value between 0V-5V
+        GoniometerValue = analogRead(GoniometerPin);
+        GoniometerVoltage = map(GoniometerValue,0,1023,0,5000);
+
+        // Read Futek load cell pin & map to a voltage value between 0V-5V
+        FutekLoadCellRawValue = analogRead(FutekLoadCellPin);
+        FutekLoadCellVoltage = map(FutekLoadCellRawValue,0,1023,0,5000);
+
+//        // 07052019 TESTING (CALIBRATION) *******************************
+//        // This block is just for calibration. It must be commented out when not performing calibration
+//
+//        int readingsPerLoop = 1500;
+//        double collectedValue = 0;
+//        int value = 0;
+//        for (int i = 0; i < readingsPerLoop; i++) {
+//          collectedValue += analogRead(FutekLoadCellPin);
+//          delay(2);
+//        }
+//
+//        double averagedValue = collectedValue / readingsPerLoop;
+////        double averagedMappedValue = map(averagedValue,0,1023,0,5000);
+//        double averagedMappedValue = map(averagedValue,0,4095,0,2.055);
+//
+//        // Printing both values to compare averaged value with individual value
+//        Serial.print(averagedMappedValue);
+//        Serial.print(" , ");
+//        Serial.println(FutekLoadCellVoltage);
+//        delay(1000);
+//        // 07092019 END *************************************************
+        
+        //for ( no = 1; no<=3; no++)
+
+//------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------
+        /* Send sensor values to LabVIEW VI */
+        Serial.print(0);     //resultant
+        
+        Serial.print(",");
+        Serial.print(); // Load cell value    //scale.getGram()
+        
+        Serial.print(",");
+        Serial.print(0); // Test value  "GoniometerVoltage"
+
+        Serial.print(",");
+        Serial.println(0); // Test value  "FutekLoadCellVoltage"
+
+//-------------------------------------------------------------------------------------
+
+//        String sensor_output = String(resultant) + "," + String(scale.getGram()) + "," + String(GoniometerVoltage) + "," + String(FutekLoadCellVoltage);
+//        Serial.println(sensor_output);
+//        delay(1000);
+
+        #endif
+
+        #ifdef Load_Cell
+        //Serial.println(scale.getValue());
+        #endif
+        
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer); 
+            Serial.print("quat\t");
+            Serial.print(q.w);
+            Serial.print("\t");
+            Serial.print(q.x);
+            Serial.print("\t");
+            Serial.print(q.y);
+            Serial.print("\t");
+            Serial.println(q.z);
+        #endif
+
+        
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+}
+
+  
